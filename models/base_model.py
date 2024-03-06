207b574da3a17c6df590487181d21a235e141de2
leila7575 <leila.louajri@gmail.com>
2024-03-06 01:49:27 +0100

    updating BaseModel with *args and **kwargs for dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,19 @@ class BaseModel():
     """
     class_name = "BaseModel"
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialisation of the attributes of the class."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Return string representation of instance."""
