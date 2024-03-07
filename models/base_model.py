5df203f7276d5d0b4ab148fd5a838fe9d40ea0cb
cely <146735843+cel3788@users.noreply.github.com>
2024-03-07 01:58:54 +0100

    updating base with variable storage

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ This module contains the class BaseModel.
 """
 import uuid
 from datetime import datetime
+from models.engine.file_storage import storage
 
 
 class BaseModel():
@@ -24,6 +25,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """Return string representation of instance."""
@@ -33,6 +35,7 @@ class BaseModel():
         """
         Updates instance attribute updated_at with current datetime."""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns dictionary of instance."""
