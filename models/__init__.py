7861b1defd24790a5325aa83fa81eda7d6672888
cely <146735843+cel3788@users.noreply.github.com>
2024-03-07 01:54:43 +0100

    udpdate init py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,10 @@
-from .base_model import BaseModel
+
+# models/__init__.py
+"""
+this module contains enry point to initialize data storage mechanisme
+"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
