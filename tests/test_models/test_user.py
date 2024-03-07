fcc313243f5bb9520a6c4e639651a69e65a248d6
cely <146735843+cel3788@users.noreply.github.com>
2024-03-07 22:32:13 +0100

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,12 +1,37 @@
-from models.base_model import BaseModel
+#!/usr/bin/python3
+"""This module defines the User class."""
 
-class User(BaseModel):
-    """User class that inherits from BaseModel"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from hashlib import md5
+
+
+class User(BaseModel, Base):
+    """A class that represents a user."""
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initialize User"""
+        """Initialize a new User."""
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """Set a password with md5 encryption."""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
