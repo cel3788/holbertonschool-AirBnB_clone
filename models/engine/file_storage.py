6c545efba6e73b3d6ca8494f11142016969f6385
cely <146735843+cel3788@users.noreply.github.com>
2024-03-07 01:48:09 +0100

    update file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,30 +1,25 @@
+# models/engine/file_storage.py
+
 import json
 import os
 
 class FileStorage:
-    """This class serializes instances to a JSON file and deserializes JSON file to instances."""
-
-    def __init__(self):
-        self.__file_path = "file.json"
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
         key = f"{type(obj).__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)."""
-        json_data = {key: value.to_dict() for key, value in self.__objects.items()}
+        serialized_objs = {key: value.to_dict() for key, value in self.__objects.items()}
         with open(self.__file_path, "w") as file:
-            json.dump(json_data, file)
+            json.dump(serialized_objs, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects."""
         if os.path.exists(self.__file_path):
             with open(self.__file_path, "r") as file:
                 try:
