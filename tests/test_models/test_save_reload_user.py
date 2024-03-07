dfd13625bce61b733a771bb48533c85b599a4dfd
cely <146735843+cel3788@users.noreply.github.com>
2024-03-07 22:11:29 +0100

    Update test_save_reload_user.py

--- tests/test_models/test_save_reload_user.py
+++ tests/test_models/test_save_reload_user.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
+"""Test save and reload for User class."""
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
 
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+print("-- Create a new User --")
+my_user = User()
+my_user.first_name = "Betty"
+my_user.last_name = "Bar"
+my_user.email = "airbnb@mail.com"
+my_user
