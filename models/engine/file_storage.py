44ec89a290baaef6b6f4b3c0b1d020ffd6c68b3e
cely <146735843+cel3788@users.noreply.github.com>
2024-03-07 22:22:43 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,33 +1,45 @@
+#!/usr/bin/python3
+"""Defines the FileStorage class."""
 import json
-import os
+from models.base_model import BaseModel
+from models.user import User
 
 class FileStorage:
+    """Represent an abstracted storage engine.
+    Attributes:
+        __file_path (str): path to the JSON file
+        __objects (dict): store all objects
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        return self.__objects
+        """Return all objects in the storage."""
+        return FileStorage.__objects
 
     def new(self, obj):
-        key = f"{type(obj).__name__}.{obj.id}"
-        self.__objects[key] = obj
+        """Add the object to the current database."""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        serialized_objs = {key: value.to_dict() for key, value in self.__objects.items()}
-        with open(self.__file_path, "w") as file:
-            json.dump(serialized_objs, file)
+        """Save the current database to the JSON file."""
+        new_dict = {}
+        for key, value in FileStorage.__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as f:
+            json.dump(new_dict, f)
 
     def reload(self):
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, "r") as file:
-                try:
-                    json_data = json.load(file)
-                    for key, value in json_data.items():
-                        class_name, obj_id = key.split('.')
-                        class_name = class_name.split('__')[-1]
-                        cls = eval(class_name)
-                        obj = cls(**value)
-                        self.__objects[key] = obj
-                except Exception as e:
-                    print("Error loading objects from JSON:", e)
-                    pass
+        """Reload the current database from the JSON file."""
+        try:
+            with open(FileStorage.__file_path, mode="r", encoding="utf-8") as f:
+                FileStorage.__objects = json.load(f)
+                for key, value in FileStorage.__objects.items():
+                    class_name = key.split('.')[0]
+                    if class_name == "BaseModel":
+                        self.__objects[key] = BaseModel(**value)
+                    elif class_name == "User":
+                        self.__objects[key] = User(**value)
+        except FileNotFoundError:
+            pass
