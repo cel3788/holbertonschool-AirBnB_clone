f0a4ac331cbdf1f1ec288bd7b665a6b9423082ed
cely <146735843+cel3788@users.noreply.github.com>
2024-03-08 12:42:22 +0100

    update models file

--- models/base_model.py
+++ models/base_model.py
@@ -1,46 +1,69 @@
 #!/usr/bin/python3
-"""
-This module contains the class BaseModel.
-"""
+"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from models.engine.file_storage import storage
+import sys
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class BaseModel():
-    """This class defines all common attributes and methods for other classes.
-    """
-    class_name = "BaseModel"
+class BaseModel:
+    """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Initialisation of the attributes of the class."""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
+        """Instatntiates a new model"""
+        self.id = str(uuid.uuid4())
+        if not kwargs:
+            from models import storage
+
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+            self.updated_at = datetime.now()
+        else:
+            if kwargs.get("created_at"):
+                kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.created_at = datetime.now()
+            if kwargs.get("updated_at"):
+                kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.updated_at = datetime.now()
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
-        """Return string representation of instance."""
-        return f"[{self.class_name}] ({self.id}) {self.__dict__}"
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """
-        Updates instance attribute updated_at with current datetime."""
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
-        """Returns dictionary of instance."""
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.class_name
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
+
+        return dictionary
+
+    def delete(self):
+        """delete the current instance from storage"""
+        from models import storage
+        storage.delete(self)
--- models/user.py
+++ models/user.py
@@ -1,39 +1,17 @@
 #!/usr/bin/python3
-"""This module defines the User class."""
-
-import models
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from hashlib import md5
+from os import getenv
 
 
 class User(BaseModel, Base):
-    """A class that represents a user."""
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initialize a new User."""
-        super().__init__(*args, **kwargs)
-
-    def __setattr__(self, name, value):
-        """Set a password with md5 encryption."""
-        if name == "password":
-            value = md5(value.encode()).hexdigest()
-        super().__setattr__(name, value)
-
-# Optimise this test.
+    """ This class defines a user by various attributes """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
