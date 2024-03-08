483ff642cb5a62726d4ab0d650e36287311d1a72
cely <146735843+cel3788@users.noreply.github.com>
2024-03-08 16:09:40 +0100

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,41 +1,16 @@
-import unittest
 import os
-import json
-import sqlalchemy
+import unittest
 import MySQLdb
+import sqlalchemy
 from io import StringIO
 from unittest.mock import patch
 from console import HBNBCommand
 from models import storage
-from models.base_model import BaseModel
 from models.user import User
-from tests import clear_stream
+
 
 class TestHBNBCommand(unittest.TestCase):
-    
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db'. 'FileStorage test')
-    def test_fs_create(self):
-        with patch('sys.stdout', new=StringIO()) as cout:
-            cons = HBNBCommand()
-            cons.onecmd('create City name="Texas"')
-            mdl_id = cout.getvalue().strip()
-            clear_stream(cout)
-            self.assertIn('City.{}'.format(mdl_id), storage.all().keys())
-            
-            cons.onecmd('show City {}'.format(mdl_id))
-            self.assertIn("'name': 'Texas'", cout.getvalue().strip())
-            clear_stream(cout)
-            
-            cons.onecmd('create User name="James" age=17 height=5.9')
-            mdl_id = cout.getvalue().strip()
-            self.assertIn('User.{}'.format(mdl_id), storage.all().keys())
-            clear_stream(cout)
-            
-            cons.onecmd('show User {}'.format(mdl_id))
-            self.assertIn("'name': 'James'", cout.getvalue().strip())
-            self.assertIn("'age': 17", cout.getvalue().strip())
-            self.assertIn("'height': 5.9", cout.getvalue().strip())
-            
+
     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DBStorage test')
     def test_db_create(self):
         with patch('sys.stdout', new=StringIO()) as cout:
@@ -112,24 +87,6 @@ class TestHBNBCommand(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as f:
             with self.assertRaises(SystemExit):
                 HBNBCommand().onecmd("quit")
-
-    def test_file_exists(self):
-        self.assertTrue(os.path.exists("file_name"))
-
-    def test_quit_present(self):
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("help quit")
-            self.assertEqual(f.getvalue().strip(), "Quitte le programme avec un formatage")
-
-    def test_EOF_present(self):
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("help EOF")
-            self.assertEqual(f.getvalue().strip(), "Quitte le programme sans formatage")
-
-    def test_help_present(self):
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("help help")
-            self.assertEqual(f.getvalue().strip(), "Affiche l'aide des commandes")
 
     def test_empty_line_present(self):
         with patch('sys.stdout', new=StringIO()) as f:
